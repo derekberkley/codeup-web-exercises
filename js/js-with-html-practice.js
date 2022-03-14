06ca65a7c8a046a4784ab97b6fe06c68f86776b3
Derek Berkley <berkley.derek@gmail.com>
2022-03-14 16:30:52 -0500

    initial upload of functions lecture notes & practice

---
+++ js/js-with-html-practice.js
@@ -0,0 +1,7 @@
+var myName = "Derek";
+var myAge = 33;
+var isMonday = true;
+
+console.log(myName, myAge, isMonday);
+console.log("I'm printing my name here: ", myName);
+console.log("My name is " + myName + " and my age is " + myAge); //concatenation
