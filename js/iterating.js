8f192ec4bbf8b2539b35ce39fbc775fa8b075e22
Derek Berkley <berkley.derek@gmail.com>
2022-03-23 14:33:07 -0500

    pushing array iteration class exercises completion

---
+++ js/iterating.js
@@ -0,0 +1,88 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+    var berkleyNames = ["Derek", "Steph", "Riley", "Apollo"];
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    console.log(berkleyNames.length);
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    console.log("Each array element printed on a line with console.log and index number.")
+    console.log(berkleyNames[0]);
+    console.log(berkleyNames[1]);
+    console.log(berkleyNames[2]);
+    console.log(berkleyNames[3]);
+
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    console.log("Use for loop to print all names in array.")
+    for (var i = 0; i < berkleyNames.length; i++) {
+        console.log(berkleyNames[i]);
+    }
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    berkleyNames.forEach(function(name) {
+        console.log("A forEach function used to print this element: " + name);
+    })
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+
+    var colorArray = ["red", "blue", "green", "yellow", "purple"];
+
+    function first(input) {
+        var final =  console.log(input[0]); // returns first item in an array
+        return final;
+    }
+
+    function second(input) {
+        var final =  console.log(input[1]); //returns second item in an array
+        return final;
+    }
+
+    function third(input) {
+        var final =  console.log(input[2]); //returns third item in an array
+        return final;
+    }
+    console.log("Array is ", colorArray);
+    first(colorArray);
+    second(colorArray);
+    third(colorArray);
+
+})();
--- js/practice-warmup.js
+++ js/practice-warmup.js
@@ -1,19 +1,59 @@
-//TODO: Write a javascript function that accepts the 3 lengths of
-// a triangle and outputs the triangle’s area.
+// //TODO: Write a javascript function that accepts the 3 lengths of
+// // a triangle and outputs the triangle’s area.
+//
+// function pythagorean(side1, side2, side3) {
+//
+//     //calculate the semi-perimeter first
+//     var semiPer = (side1 + side2 + side3) / 2;
+//
+//     //using Heron's Formula since only know 3 sides of triangle
+//     var area = Math.sqrt(semiPer * (semiPer - side1) * (semiPer - side2) * (semiPer - side3));
+//
+//     //Area of Triangle Formula if had other info
+//     // var triArea = (base * height) * 0.5
+//
+//     console.log("The area of the triangle is: " + area);
+//     return area;
+// }
+//
+// pythagorean(3, 4, 5);
 
-function pythagorean(side1, side2, side3) {
+//TODO: Fizzbuzz Activity
+// Print numbers 1-100
+//if # is divisible by 3 use Fizz
+// if # is divisible by 5 use Buzz
+// if # is divisible by both 3 and 5 use FizzBuzz
 
-    //calculate the semi-perimeter first
-    var semiPer = (side1 + side2 + side3) / 2;
-
-    //using Heron's Formula since only know 3 sides of triangle
-    var area = Math.sqrt(semiPer * (semiPer - side1) * (semiPer - side2) * (semiPer - side3));
-
-    //Area of Triangle Formula if had other info
-    // var triArea = (base * height) * 0.5
-
-    console.log("The area of the triangle is: " + area);
-    return area;
+function fizzBuzz() {
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log(i + " Fizz");
+        } else if (i % 5 === 0) {
+            console.log(i + " Buzz");
+        } else {
+            console.log(i); //print #'s 1-100
+        }
+    }
 }
 
-pythagorean(3, 4, 5);
+fizzBuzz();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
