7eccf752d04aa3bcba892a3fe3c3a3c29c6d73bd
Derek Berkley <berkley.derek@gmail.com>
2022-04-06 09:36:41 -0500

    adding work on morning warmup

--- js/practice-warmup.js
+++ js/practice-warmup.js
@@ -306,3 +306,32 @@ function sumArr(arry) {
 // sumArr(emptyArray); //testing empty array output (correct string return)
 // sumArr(strArray); //testing numbers passed as word strings (returns NaN)
 // sumArr(numStrArr); //testing numbers passed as strings (returns 15 correctly)
+
+//TODO: Create a function to sort the array from lowest to highest number
+// example input: [5,8,2,6,9,3,2];
+// expected output: [2,2,3,5,6,8,9] // length: 7
+// possible test cases to think about:
+// should be called ‘sortArr’
+// function should return an array
+// function should accept an array
+
+var exampleArray = [9, 5, 3, -6, 0, -23, .5, -2.5];
+
+function sortArr(input) {
+    input.forEach(function(element) {
+        if (input.indexOf(element) !== input.lastIndexOf(element)) {
+
+        }
+    })
+    var final = input.sort((a, b) => a - b);
+    console.log(final);
+    return final;
+}
+sortArr(exampleArray);
+
+// TODO: BONUS thoughts
+//  what would happen if you had negative numbers in your list
+//  can you sort and then remove any duplicates (would it be more/less
+//  efficient to remove duplicates and then sort?)
+//  if your array has non-numbers can you still sort the numbers?
+//  ex: input: [3,“b”,8,5,9,true”, 4,“xyz”] expected output: [3,4,5,8,9];
