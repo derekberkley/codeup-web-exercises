a0d06b2df450795309dd5081d17a3318270e35e9
Derek Berkley <berkley.derek@gmail.com>
2022-04-14 13:05:33 -0500

    adding progress on pizza world exercise, previous and next buttons are troublesome

--- js/practice-warmup.js
+++ js/practice-warmup.js
@@ -434,3 +434,25 @@ function merge(arry1, arry2) {
 };
 
 merge(randomThings, moreRandoThings);
+
+
+// Write a function that takes an array of objects as argument
+// Sort the array by property b in ascending order
+// Return the sorted array
+
+var tryMe = [{a:1,b:2},{a:5,b:4}]
+// expected output: [{a:1,b:2},{a:5,b:4}]
+
+function sortThisThing(arrObj) {
+    arrObj.sort(function(arrObj[a].b, arrObj[b].b);
+    {
+        return arrObj[a].b - arrObj[b].b;
+    };
+)};
+    sortThisThing(tryMe);
+
+    array.sort(x, y) {
+    if(x.b < y.b) {
+        return -1;
+    }
+    }
