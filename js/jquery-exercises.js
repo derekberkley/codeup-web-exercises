88b14c85d494468bdd179cff6a23c23e3a04da16
Derek <99042983+derekberkley@users.noreply.github.com>
2022-04-27 15:35:53 -0500

    completed jQuery mouse-click exercises

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -6,7 +6,7 @@
 //****************************************************
 
 //****************************************************
-//testing jQuery selectors with an alert (two syntax methods)
+// TODO: Create a jQuery statement to alert the contents of your h1 element(s).
 // alert($("#page-title").html());
 //
 //OR written as...
@@ -26,13 +26,43 @@ $(".codeup").css("border", "1px solid red");
 
 
 //****************************************************
-// changes font size for multiple instances of a single element type
+// TODO: Using jQuery, set the font-size of all li elements to 20px.
 $("li").css("font-size", "20px");
 //****************************************************
 
 
 //****************************************************
-// use multiple selectors to highlight each type of element
+// TODO: Craft selectors that highlight all the h1, p, and li elements.
 // highlights the entire element NOT just the text within.
 $("h1, p, li").css("background-color", "yellow");
+//****************************************************
+
+//****************************************************
+// TODO: Add jQuery code that will change the background color
+//  of an h1 element when clicked.
+$("h1").click(
+    function() {
+        $(this).css('background-color', 'blue');
+    }
+);
+//****************************************************
+
+//****************************************************
+// TODO: Make all paragraphs have a font size of 18px when
+//  they are double clicked.
+$("p").dblclick(
+    function() {
+        $(this).css('font-size', '18px');
+    }
+);
+//****************************************************
+
+//****************************************************
+// TODO: Set all li text color to red when the mouse is
+//  hovering; reset to black when it is not.
+$("li").hover(
+    function() {
+        $(this).css('color', 'red');
+    }
+);
 //****************************************************
