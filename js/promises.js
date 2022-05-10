5d3537d454eb7f9a259229f7a98edb73e35a3f5d
Derek <99042983+derekberkley@users.noreply.github.com>
2022-05-10 16:02:07 -0500

    promises Ex: receiving correct data from fetch, need to pull last commit from array of objs

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,26 @@
-// const githubToken = "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";
 
-fetch(url, {headers: {'Authorization': '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'}})
+// TODO: Create a file named promises.js inside of your js directory
+// and link it to an HTML file of your choice.
 
-function grabUsername(uName) {
+// TODO: Generate a Personal Access Token for the GitHub API.
+//  We will use this so that we don't get rate limited when talking to
+//  the GitHub API. You can add the token to your requests like this:
+//  fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
 
-//    return date of last commit
+// TODO: Create a function that accepts a GitHub username, and returns a promise
+//  that resolves returning just the date of the last commit that user made.
+//  Reference the github api documentation to achieve this.
 
-}
+
+
+
+
+    const user = prompt("What is your github username?");
+
+    fetch(`https://api.github.com/users/${user}/events`, {headers: {'Authorization': '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'}})
+        .then(response => response.json())
+        .then(jsonData => console.log("response(jsonData): ", jsonData))
+        // .then(jsonData)
+        .catch(error => console.error("error: ", error));
+
+
